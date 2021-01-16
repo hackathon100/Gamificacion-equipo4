d1be54e8435c1ab556da0797b65a1989a6becf70
Jonnatan Montt Villar <jonnatan_montt@hotmail.com>
2021-01-16 12:14:43 -0300

    Base Vue CLI

---
+++ src/components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  props: {
+  }
+}
+</script>
+
+
+<style scoped>
+#nav {
+  /*background:rgb(235, 97, 72);  Aqui Background navbar */
+  padding: 10px; 
+  text-align: end;  
+}
+
+#nav a {  
+  margin-right: 2rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import navbar from '@/components/navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //navbar
+  }
+}
+</script>
