31686384aeeaf29121c26add9e5614bed8eacc45
Alexis Ordenes <giraalexis@outlook.com>
2021-01-16 20:04:36 -0300

    Agregar Base Simple

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const SocketIO = require('socket.io');
+
+//settings:
+app.set('port', process.env.PORT  || 3000);
+
+//static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+//start the server
+const server = app.listen(app.get('port'), ()=>{
+    console.log('server on port', app.get('port'));
+});
+
+//init socket
+const io = SocketIO(server);
+
+
+//Variables
+
+//Web Socket
+io.on('connection',(socket)=>{
+    //al Revicir nueva conexion:
+    console.log("new conecction", socket.id);
+    io.sockets.emit('join:User',socket.id);
+
+    
+})
